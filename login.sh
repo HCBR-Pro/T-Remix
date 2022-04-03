773daab56b889e76e1c62afbd1d0a51ad42789e6
HCBR-PRo <85325988+HCBR-Pro@users.noreply.github.com>
2022-04-03 11:22:01 -0300

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,9 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # T-Remix password login setup
 #########################################
-#         Project : T-Remix             #
-#          Author: Ashish               #
-#           T-Remix 2.0                 #
+#         Project : Termux-2022         #
+#          Author: KaLi-_-LiNuX         #
+#            TermuX 2.0                 #
 #########################################
 
 # abort the login if python is not installed 
@@ -45,7 +45,7 @@ then
  echo
 else
  echo
- echo "\e[31m Python not found\e[m"
+ echo "\e[31m Python não encontrado\e[m"
  echo
  echo "\e[31m Re-run :\e[32mbash t-remix.sh \e[m"
  exit 0
@@ -55,19 +55,19 @@ fi
 tput civis
 echo -e "\e[1;33m☆\e[1;32m:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\e[1;33m☆\e[m "
 echo 
-echo -e "\e[34m[*]\e[m \e[33m Please wait \e[m"
+echo -e "\e[34m[*]\e[m \e[33m Por favor, aguarde \e[m"
 echo
 sleep 3
-echo -e "\e[34m[*]\e[m \e[33m Copying Login file \e[m"
+echo -e "\e[34m[*]\e[m \e[33m Copiando arquivo de login \e[m"
 echo
 sleep 2
-echo -e "\e[34m[*]\e[m \e[33m Setting up user input \e[m"
+echo -e "\e[34m[*]\e[m \e[33m Configurando a entrada do usuário \e[m"
 echo
 sleep 3
-echo -e "\e[34m[*]\e[m \e[33m Create a password \e[m"
+echo -e "\e[34m[*]\e[m \e[33m Crie uma senha \e[m"
 echo
 sleep 2
-echo -e "\e[34m[*]\e[m \e[33m Save user input \e[m"
+echo -e "\e[34m[*]\e[m \e[33m Salvar entrada do usuário \e[m"
 echo 
 sleep 2
 echo
@@ -76,20 +76,20 @@ tput cnorm
 # start a while loop if password login fail
 while :
 do
-read -p $'\e[36m>>\e[32m Enter password for login :\e[m ' passone
+read -p $'\e[36m>>\e[32m Digite a senha para login :\e[m ' passone
 echo
 sleep 1
-read -p $'\e[36m>>\e[32m Repeat password :\e[m ' passtwo
+read -p $'\e[36m>>\e[32m Repita a senha :\e[m ' passtwo
 sleep 2
 echo
 if [ $passone = $passtwo ];
 then
 	touch /data/data/com.termux/files/usr/share/login/.pass
 	python -c "import hashlib; print(hashlib.sha1(b'$passone').hexdigest())" > /data/data/com.termux/files/usr/share/login/.pass
-echo -e "\e[31m[*]\e[32m Login succesfully installed \e[m"
+echo -e "\e[31m[*]\e[32m Login instalado com sucesso \e[m"
 break ;
 else 
-echo -e "\e[33m[\e[31m!\e[33m]\e[31m Password doesn't match try again \e[m"
+echo -e "\e[33m[\e[31m!\e[33m]\e[31m A senha não corresponde tente novamente \e[m"
 sleep 2
 clear
 echo
